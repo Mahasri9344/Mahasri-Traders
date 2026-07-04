a672a09b87badd9ad749f8d2a9e0a5b70274e3e8
Arun Kumar <arun@example.com>
2026-07-04 15:43:17 +0530

    Added product search and categories

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 console.log("Mahasri Traders Website Loaded");
+
+const searchBox = document.getElementById("searchBox");
+const cards = document.querySelectorAll(".card");
+const productCount = document.getElementById("productCount");
+
+searchBox.addEventListener("keyup", function () {
+
+    const searchText = searchBox.value.toLowerCase();
+
+    let visibleProducts = 0;
+
+    cards.forEach(function (card) {
+
+        const productName = card.dataset.name.toLowerCase();
+
+        if (productName.includes(searchText)) {
+            card.style.display = "block";
+            visibleProducts++;
+        } else {
+            card.style.display = "none";
+        }
+
+    });
+
+    productCount.textContent = `Showing ${visibleProducts} products`;
+
+});
